19c4e9758af4759baee37f644b2f6c5d9b4c031a
Ayoub Sadouri <135533101+Frontendab@users.noreply.github.com>
2024-02-18 15:25:16 +0100

    handling.jsx

--- src/components/navbar/Handling navbar links/handling.jsx
+++ src/components/navbar/Handling navbar links/handling.jsx
@@ -6,6 +6,7 @@ import { links } from "@/asset/data/data";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
 
+export let widthWindows = window.innerWidth;
 const HandlingNavBarLinks = () => {
   const [currentIndexLink, setCurrentIndexLink] = useState();
 
@@ -22,57 +23,60 @@ const HandlingNavBarLinks = () => {
 
   return (
     <>
-        <div className={styles.ON_MAX_MD}>
-          <FaBarsStaggered
-            style={{
-              fontSize: "40px",
-              color: "white",
-              marginRight: "12px",
-              cursor: "pointer",
-            }}
-            onClick={() => {
-              setConditionMenu(!conditionMenu);
-            }}
-          />
-          <div
-            className={`${conditionMenu ? styles.wapperMenuLinks : "d-none"}`}
-          >
-            <IoMdClose
-              className={styles.iconClose}
+      {widthWindows<=991? (
+        <div>
+            <FaBarsStaggered
+              style={{
+                fontSize: "40px",
+                color: "white",
+                marginRight: "12px",
+                cursor: "pointer",
+              }}
               onClick={() => {
                 setConditionMenu(!conditionMenu);
               }}
             />
-            <ul>
-              {links.map((link, i) => (
-                <Link key={i} href={link.path}>
-                  <li
-                    onClick={() => handlingLinks(i)}
-                    className={`text-capitalize ${
-                      currentIndexLink === i ? styles.active : ""
-                    } ${i === links.length - 1 ? styles.last_child : ""}`}
-                  >
-                    {link.link} {link.icon}
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </div>
+            <div
+              className={`${conditionMenu ? styles.wapperMenuLinks : "d-none"}`}
+            >
+              <IoMdClose
+                className={styles.iconClose}
+                onClick={() => {
+                  setConditionMenu(!conditionMenu);
+                }}
+              />
+              <ul>
+                {links.map((link, i) => (
+                  <Link key={i} href={link.path}>
+                    <li
+                      onClick={() => handlingLinks(i)}
+                      className={`text-capitalize ${
+                        currentIndexLink === i ? styles.active : ""
+                      } ${i === links.length - 1 ? styles.last_child : ""}`}
+                    >
+                      {link.link} {link.icon}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
         </div>
-        <ul className={`d-flex align-items-center ${styles.ON_MIN_MD}`}>
-          {links.map((link, i) => (
-            <Link key={i} href={link.path}>
-              <li
-                onClick={() => handlingLinks(i)}
-                className={`text-capitalize ${
-                  currentIndexLink === i ? styles.active : ""
-                } ${i === links.length - 1 ? styles.last_child : ""}`}
-              >
-                {link.link} {link.icon}
-              </li>
-            </Link>
-          ))}
-        </ul>
+      ) : (
+          <ul className='d-flex align-items-center'>
+            {links.map((link, i) => (
+              <Link key={i} href={link.path}>
+                <li
+                  onClick={() => handlingLinks(i)}
+                  className={`text-capitalize ${
+                    currentIndexLink === i ? styles.active : ""
+                  } ${i === links.length - 1 ? styles.last_child : ""}`}
+                >
+                  {link.link} {link.icon}
+                </li>
+              </Link>
+            ))}
+          </ul>
+      )}
     </>
   );
 };
